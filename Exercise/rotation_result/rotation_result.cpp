1ce336a41243bbbc7c0042a6010e594cedcfdc56
Zekeriri <2431490884@qq.com>
2023-02-17 17:03:23 +0800

    2-17

--- Exercise/rotation_result/rotation_result.cpp
+++ Exercise/rotation_result/rotation_result.cpp
@@ -31,3 +31,11 @@ void spin(char* str, int k)
 	}
 	str[k - 1] = temp;
 }
+
+int findRound(const char* src, char* find)
+{
+	char tmp[256] = { 0 }; //用一个辅助空间将原字符串做成两倍原字符串
+	strcpy(tmp, src); //先拷贝一遍
+	strcat(tmp, src); //再连接一遍
+	return strstr(tmp, find) != NULL; //看看找不找得到
+}
